179daf5b6a9223e8a02bf3ee403a3f6d7fdf7411
sneyder53 <sneyderamado.com@gmail.com>
2022-04-24 15:22:06 -0500

    primer commit

---
+++ src/main/java/com/fundamentos/springboot/fundamientos/FundamientosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.fundamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamientosApplication.class, args);
+	}
+
+}
